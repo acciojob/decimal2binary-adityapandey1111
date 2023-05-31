32ba88eca46fed4b36c21f34f7962d703a720b25
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-05-31 12:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function decimalToBinary(decimalNumber) {
+  // Check if the input is 0
+  if (decimalNumber === 0) {
+    return '0';
+  }
 
-module.exports = threeSum;
+  // Create an empty array to store the binary digits
+  const binaryArray = [];
+
+  // Convert the decimal number to binary
+  while (decimalNumber > 0) {
+    // Get the remainder of division by 2
+    const remainder = decimalNumber % 2;
+
+    // Add the remainder to the binary array
+    binaryArray.unshift(remainder);
+
+    // Update the decimal number by dividing it by 2
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  // Join the binary digits into a string and return it
+  return binaryArray.join('');
+}
